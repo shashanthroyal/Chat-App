b297130452c6f07efcef32174ff433784800833c
shashanthroyal <shashanth.chandu@gmail.com>
2025-09-09 10:57:16 +0530

    added the path

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import './Auth.css';
+import './styles/Auth.css';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
